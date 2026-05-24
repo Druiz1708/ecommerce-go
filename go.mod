b7eb7066f85a93750559e08bb3631aa6e62ab117
Daysi <dr562582mail.com>
2026-05-24 19:12:04 -0400

    Primer avance ecommerce

---
+++ go.mod
@@ -0,0 +1,3 @@
+module TrabajoAutonomo
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+	El import "html/template" es la carpeta de template
+	para que go
+	muestre la pagina de template, ya que html solo es
+	lo visual pero go es quien abre la pagina y lo conecta
+*/
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func inicio(w http.ResponseWriter, r *http.Request) {
+	pagina, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "No se pudo cargar la pagina", http.StatusInternalServerError)
+		fmt.Println("Error:", err)
+		return
+	}
+
+	pagina.Execute(w, nil)
+
+}
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", inicio)
+	http.HandleFunc("/productos", productos)
+	fmt.Println("Servidor inciciado en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+func productos(w http.ResponseWriter, r *http.Request) {
+	pagina, _ := template.ParseFiles("templates/productos.html")
+	pagina.Execute(w, nil)
+}
